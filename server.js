17aa13ee285c121c8e05362963ff6ef79a4910b1
Fannypc <mars.poc@gmail.com>
2021-01-10 21:56:25 -0600

    UPDATE: agregando base datos en heroku

--- server.js
+++ server.js
@@ -5,6 +5,27 @@ const cors = require('cors');
 const app = express(); // creating instance
 const cookieParser = require('cookie-parser');
 const cool = require('cool-ascii-faces');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+app.get('/db', async(req, res)=> {
+    try{
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM test_table');
+        const results = {'results': (result) ? result.rows: null};
+        res.render('pages/db', results);
+        client.release();
+    }catch(err){
+        console.log(err);
+        res.send('Error '+err);
+    }
+})
 
 //Import Routes
 const authRouter = require('./routes/auth');
