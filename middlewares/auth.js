6cf3a2a71febb382c8d70398afd8f63ea06c124c
Fannypc <mars.poc@gmail.com>
2021-02-11 12:24:46 -0600

    UPDATE: eliminando comentarios

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -23,24 +23,6 @@ const validateToken = (request, response, next) => {
         }
     }
 
-    // const token = request.cookies.access_token;
-    // console.log(token);
-    // if(token){
-    //     try {
-    //         request['token'] = token;
-    //         let decoded = jwt.verify(token, process.env.JWT_SECRET);
-    //         request['decoded'] = decoded;
-    //         // if token can be decoded then user is valid
-    //         if(decoded){
-    //             next();
-    //         }
-    //     } catch(err) {
-    //         console.log(err);
-    //         response.status(401).json({message: "El token es invalido"})
-    //     }
-    // }else{
-    //     response.status(401).json({message: "El token es invalido"})
-    // }
 }
 
 module.exports = validateToken;
